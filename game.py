32d1ee616a00dd0779530869f13e70c03bacb0a9
izon21 <154813407+izon21@users.noreply.github.com>
2023-12-25 19:51:12 +0500

    Add files via upload

--- game.py
+++ game.py
@@ -28,6 +28,46 @@ class Player(GameSprite):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
+    def fire(self):
+        bullet = Bullet('algo.py\пуля.jpg', self.rect.right, self.rect.centery, 20,10, 15)
+        bullets.add(bullet)
+
+
+class Enemy(GameSprite):
+    side = 'left'
+    def __init__(self, sprite_image, x, y, size_x, size_y, enemy_speed):
+        GameSprite.__init__(self, sprite_image, x, y, size_x, size_y)
+        self.speed = enemy_speed
+        self.side = 'left'
+    def update(self):
+        if self.rect.x >= 650:
+            self.side = 'left'
+        if self.rect.x <= 320:
+            self.side = 'right'
+        if self.side == 'right':
+            self.rect.x += self.speed
+        if self.side == 'left':
+            self.rect.x -= self.speed
+
+
+class Bullet(GameSprite):
+    def __init__(self, sprite_image, x, y, size_x, size_y, bullet_speed):
+        GameSprite.__init__(self, sprite_image, x, y, size_x, size_y)
+        self.speed = bullet_speed
+    def update(self):
+         self.rect.x += self.speed
+         if self.rect.x > 700:
+             self.kill()
+
+
+monster1 = Enemy('algo.py\сквидват.png', 640, 30, 60, 60, 5 )
+monster2 = Enemy('algo.py\сквидват.png', 640, 130, 60, 60, 5 )
+monster3 = Enemy('algo.py\сквидват.png', 640, 230, 60, 60, 5 )
+monsters = sprite.Group()
+monsters.add(monster1)
+monsters.add(monster2)
+monsters.add(monster3)
+
 wall1 = GameSprite('algo.py\мега стена.png',100, 100, 20, 400)
 wall2 = GameSprite('algo.py\мега стена.png',200, 0, 20, 400)
 wall3 = GameSprite('algo.py\мега стена.png',300, 100, 20, 400)
@@ -36,8 +76,29 @@ wall5 = GameSprite('algo.py\мега стена.png',320, 200, 300,20)
 wall6 = GameSprite('algo.py\мега стена.png',400, 300, 400,20)
 wall7 = GameSprite('algo.py\мега стена.png',320, 400, 300,20)
 
-player = Player('algo.py\патрик.jpg', 10, 420, 50, 50, 0, 0)
+
+player = Player('algo.py\патрик.jpg', 15, 420, 50, 50, 0, 0)
 run = True
+finish = False
+
+walls = sprite.Group()
+walls.add(wall1)
+walls.add(wall2)
+walls.add(wall3)
+walls.add(wall4)
+walls.add(wall5)
+walls.add(wall6)
+walls.add(wall7)
+
+bullets = sprite.Group()
+
+def game_over():
+    game_over_image = transform.scale(image.load('algo.py\мега стена.png'), (700, 500))
+    window.blit(game_over_image, (0, 0))
+    player.speed_x = 0
+    player.speed_y = 0
+    finish = True
+
 while run:
     time.delay(50)
 
@@ -55,6 +116,9 @@ while run:
                 player.speed_x = -5
             elif e.key == K_d:
                 player.speed_x = 5
+            elif e.key == K_SPACE:
+                player.fire()
+
         elif e.type == KEYUP:
             if e.key == K_w:
                 player.speed_y = 0
@@ -62,27 +126,42 @@ while run:
                 player.speed_y = 0
             elif e.key == K_a:
                 player.speed_x = 0
-            elif e.key == K_a:
+            elif e.key == K_d:
                 player.speed_x = 0
+        
+    
 
-    if player.rect.x >= 340 and player.rect.y >= 400:
-        game_over_image = transform.scale(image.load('algo.py\мега стена.png'), (700, 500))
-        window.blit(game_over_image, (0, 0))
-        run = False
+    if finish != True:
+        walls.draw(window)
+        monsters.draw(window)
+        bullets.draw(window)
+        player.reset()
+        player.update()
+        bullets.update()
 
-    if sprite.collide_rect(player, wall1) or sprite.collide_rect(player, wall2) or sprite.collide_rect(player, wall3) or sprite.collide_rect(player, wall4) or sprite.collide_rect(player, wall5) or sprite.collide_rect(player, wall6) or sprite.collide_rect(player, wall7):
-        player.speed_x = 0
-        player.speed_y = 0
+        sprite.groupcollide(monsters, bullets, True, True)
+        sprite.groupcollide(walls, bullets, False, True)
+        if (player.rect.x >= 700 or player.rect.x <= 0) or (player.rect.y >= 500 or player.rect.y <= 0):
+            game_over()
+            
 
-    wall1.reset()
-    wall2.reset()
-    wall3.reset()
-    wall4.reset()
-    wall5.reset()
-    wall6.reset()
-    wall7.reset()
-    player.reset()
-    player.update()
+        
+        if sprite.spritecollide(player, walls, False):
+            player.speed_x = 0
+            player.speed_y = 0
+            game_over()
+        
+        if sprite.spritecollide(player, walls, False):
+            player.speed_x = 0
+            player.speed_y = 0
+            game_over()
+        
+
+
+
+        monsters.update()
+    
+
     display.update()
 
 
